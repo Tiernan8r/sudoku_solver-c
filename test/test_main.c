0d4545b7974449609bf709ef7b4ef373eb47a2cf
Tiernan8r <tiernan8r@pm.me>
2022-12-19 16:25:26 +0000

    placeholder files

---
+++ test/test_main.c
@@ -0,0 +1,44 @@
+// MIT License
+//
+// Copyright (c) 2022 Tiernan8r
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+#include "entry.h"
+#include "debug.h"
+#include "unity.h"
+
+void setUp(void) {}    /* Is run before every test, put unit init calls here. */
+void tearDown(void) {} /* Is run after every test, put unit clean-up calls here. */
+
+void test_usage(void)
+{
+}
+
+void test_main(void) {
+    
+}
+
+int main(void)
+{
+    UNITY_BEGIN();
+
+    RUN_TEST(test_testBoard);
+
+    return UNITY_END();
+}
